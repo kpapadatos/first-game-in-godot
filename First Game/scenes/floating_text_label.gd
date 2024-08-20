6fac53b2532c19e83f695c582821c17b2ad1383e
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2024-08-21 00:11:29 +0300

    asd

--- First Game/scenes/floating_text_label.gd
+++ First Game/scenes/floating_text_label.gd
@@ -3,7 +3,3 @@ extends Label
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$AnimationPlayer.play("default")
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
--- First Game/scenes/pickup_radius.gd
+++ First Game/scenes/pickup_radius.gd
@@ -8,5 +8,5 @@ var radius = 0
 func _ready() -> void:
 	collision_shape.shape.radius = radius
 
-func _on_area_shape_entered(area_rid: RID, item: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+func _on_area_shape_entered(_area_rid: RID, item: Area2D, _area_shape_index: int, _local_shape_index: int) -> void:
 	item.attract(unit)
