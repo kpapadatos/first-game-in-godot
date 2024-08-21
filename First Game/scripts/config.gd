9a5215ee67457d426797bafd043926ffb4c9badc
Kosmas Papadatos <kosmas.papadatos@gmail.com>
2024-08-22 02:18:59 +0300

    asd

--- First Game/scripts/config.gd
+++ First Game/scripts/config.gd
@@ -5,6 +5,13 @@ var file_name = "user://state.cfg"
 
 func _ready() -> void:
 	config_file.load(file_name)
+	
+func _input(event):
+	if Input.is_action_just_pressed("toggle_fullscreen"):
+		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		elif DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_FULLSCREEN:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 
 func get_value(section: String, key: String):
 	return config_file.get_value(section, key)
